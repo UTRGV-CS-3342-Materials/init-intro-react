eae05e6bf0cc9aa86e3bc7ff514a35111b7484f1
Emmett Tomai <emmett.tomai@utrgv.edu>
2026-09-03 10:42:00 -0500

    checkpoint: Vn

---
+++ inclass/app/Hello.jsx
@@ -0,0 +1,18 @@
+export function Hello() {
+
+    // get data
+
+    // respond to user actions
+
+    // do stuff with data
+
+    return (<></>);
+    //     <>
+    //         <h1>Hi</h1>
+    //         <ul>
+    //             <li>1</li>
+    //             <li>2</li>
+    //         </ul>
+    //     </>
+    // );
+}
